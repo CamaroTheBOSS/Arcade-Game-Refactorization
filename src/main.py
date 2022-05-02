377049714e6630161187c18b26946d09d562a5f4
Kacper Plesiak <plesiakmail@gmail.com>
2022-05-02 19:29:46 +0200

    Main menu has been added. Choose level menu still in progress

--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ import pygame
 from game.game import Game
 from editor.leveleditor import LevelEditor
 from game.summary import SummaryWindow
-import sys
-import os
+from menus.menu import Menu
 
 
 class PyWindow:
@@ -27,53 +26,18 @@ class PyWindow:
         self.summaryWindow.show()
 
 
-class Menu:
-    def __init__(self, window):
-        self.window = window
-        self.optionPointer = 0
-        self.options = []
-        self.running = False
-        self.clock = pygame.time.Clock()
-
-    def selectUp(self):
-        if self.optionPointer >= len(self.options) - 1:
-            self.optionPointer = 0
-        else:
-            self.optionPointer += 1
-
-    def selectDown(self):
-        if self.optionPointer == 0:
-            self.optionPointer = len(self.options) - 1
-        else:
-            self.optionPointer -= 1
-
-    def update(self):
-        pass
-
-    def run(self):
-        self.running = True
-
-        # Editor main loop
-        while self.running:
-            self.clock.tick(60 * 3)
-            # Check for events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP or event.key == pygame.K_w:
-                        self.selectUp()
-                    elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                        self.selectDown()
-
-            self.update()
-
-
 if __name__ == '__main__':
     app = PyWindow()
-    # app.levelEditor.run()
+    x = "menu"
+    while True:
+        if x == "menu":
+            x = app.menuWindow.run()
+        elif x == "chooseLevel":
+            pass
+        elif x == "editLevel":
+            app.levelEditor.run()
 
-    app.loadAndRunLevel("1.txt")
-    app.showSummaryWindow()
-    app.loadAndRunLevel("2.txt")
-    app.showSummaryWindow()
+        # app.loadAndRunLevel("1.txt")
+        # app.showSummaryWindow()
+        # app.loadAndRunLevel("2.txt")
+        # app.showSummaryWindow()
---
+++ src/menus/menu.py
@@ -0,0 +1,75 @@
+import pygame
+import sys
+from data.HUD import HUDelement
+
+
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        self.optionPointer = 0
+        self.options = HUDelement()
+        self.font = pygame.font.SysFont('Calibri', 30, bold=True)
+        self.options.textToRender = [
+            self.font.render(f"Start game", True, (255, 255, 255)),
+            self.font.render(f"Choose level from memory", True, (255, 255, 255)),
+            self.font.render(f"Quit", True, (255, 255, 255))
+        ]
+        self.colors = [(0, 0, 0), (255, 255, 255), (255, 255, 255)]
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+    def selectDown(self):
+        if self.optionPointer >= len(self.options.textToRender) - 1:
+            self.optionPointer = 0
+        else:
+            self.optionPointer += 1
+
+        self.colors[self.optionPointer] = (0, 0, 0)
+        self.colors[self.optionPointer - 1] = (255, 255, 255)
+
+    def selectUp(self):
+        if self.optionPointer == 0:
+            self.optionPointer = len(self.options.textToRender) - 1
+            self.colors[self.optionPointer] = (0, 0, 0)
+            self.colors[0] = (255, 255, 255)
+        else:
+            self.optionPointer -= 1
+            self.colors[self.optionPointer] = (0, 0, 0)
+            self.colors[self.optionPointer + 1] = (255, 255, 255)
+
+    def update(self):
+        self.window.fill((23, 125, 105))
+        self.options.textToRender = [
+            self.font.render(f"Start game", True, self.colors[0]),
+            self.font.render(f"Edit level", True, self.colors[1]),
+            self.font.render(f"Quit", True, self.colors[2])
+        ]
+        for i, option in enumerate(self.options.textToRender):
+            self.window.blit(option, (50, 50 + i * 50))
+        pygame.display.flip()
+
+    def run(self):
+        self.running = True
+
+        # Editor main loop
+        while self.running:
+            self.clock.tick(60 * 3)
+            # Check for events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        self.selectUp()
+                    elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        self.selectDown()
+                    elif event.key == pygame.K_RETURN:
+                        if self.optionPointer == 0:
+                            print("start")
+                            return "chooseLevel"
+                        elif self.optionPointer == 1:
+                            print("edit level")
+                            return "editLevel"
+                        elif self.optionPointer == 2:
+                            sys.exit()
+            self.update()
